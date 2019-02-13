41f24428ef2d2e12e6fdc0a553ba7b6194526aad
Eric Kim <hyeokjungkim@gmail.com>
2019-02-12 19:10:12 -0500

    fix readme to better document 021218 meeting

--- bot/commands.rb
+++ bot/commands.rb
@@ -1,13 +1,18 @@
 module Flombot
   module Commands
     class Taco < SlackRubyBot::Commands::Base
+      # Command: You have to directly @Flombot
       command 'who sent this' do |client, data, match|
         sender_id = data["user"]
         sender_info = client.users.find{|id, information_array| id == sender_id}[1].profile
-        client.say(text:"hello, #{sender_info.display_name}!", channel:data.channel)
+        client.say(text:"hello, #{sender_info.display_name}!", channel: data.channel)
+        #Optional Argument in client.say: thread_ts: data.thread_ts || data.ts to reply to thread
       end
 
+      # Listens to taco
       scan /(:taco:)/ do |client, data, match|
+        # byebug
+        # Better parsing
         recievers = data.text.scan(/\@\w+/)
         users = recievers.map do |id|
           user = User.find_by(slack_id: id[1..-1])
@@ -15,7 +20,7 @@ module Flombot
             user.count +=1
             user.save
           else
-            information = client.users.find{|s_id, information| id[1..-1] == s_id}[1].profile
+            information = client.users.find{|slack_id, information| id[1..-1] == slack_id}[1].profile
             user = User.create(
               slack_id: id[1..-1],
               name: information.display_name,
@@ -23,9 +28,9 @@ module Flombot
           end
           user
         end
-        user_names = users.map { |user| user.name  }
+        user_names = users.map { |user| "<@#{user.slack_id}>"  }
         user_shoutouts = users.map { |user| user.count  }
-        client.say(channel:data.channel, text: "#{user_names.join(', ')} has recieved your shoutouts! #{user_shoutouts.join(', ')}")
+        client.say(channel:data.channel, text: "#{user_names.join(', ')} has received your shoutouts! #{user_shoutouts.join(', ')}")
       end
     end
   end
